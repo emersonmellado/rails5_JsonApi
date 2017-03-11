a09a2b581b168401663fff1bedd7b72dfe31ce43
Emerson Mellado <emersonmellado@gmail.com>
2017-03-11 00:28:34 -0800

    Removing Products table for clean up

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311081423) do
+ActiveRecord::Schema.define(version: 20170311082632) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "title"
     t.string   "description"
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
 end
