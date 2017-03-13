910481198a70dc54368bc7e6114449bb4174960a
Emerson Mellado <emersonmellado@gmail.com>
2017-03-12 23:16:41 -0700

    Adding auth in a private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::API
 	include ActionController::HttpAuthentication::Basic::ControllerMethods
 	include ActionController::Serialization
-	http_basic_authenticate_with username: "erm", password: "pass"
+	before_filter :authenticate
 
+	def authenticate
+		authenticate_or_request_with_http_basic('Administration') do |username, password|
+		  username == 'admin' && password == 'password'
+		end
+	end
 end
