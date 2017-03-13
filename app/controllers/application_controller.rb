338c0aba40eb4b32f1f1b4ef07d7debef9198a2a
Emerson Mellado <emersonmellado@gmail.com>
2017-03-12 23:22:55 -0700

    Implementing md5 on the password for the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
+require 'digest'
+
 class ApplicationController < ActionController::API
 	include ActionController::HttpAuthentication::Basic::ControllerMethods
-	include ActionController::Serialization
+	include ActionController::Serialization 	
 	before_filter :authenticate
 
 	def authenticate
 		authenticate_or_request_with_http_basic('Administration') do |username, password|
-		  username == 'admin' && password == 'password'
+			md5_of_password = Digest::MD5.hexdigest(password)
+			username == 'admin' && md5_of_password == 'd3e5b482609e714465ac95093e248d8f'
 		end
 	end
 end
